3bed1ed7f25367ac2c9df4d3eeef1e02c7245f18
huaminni <huamin@huaminnideMacBook-Pro.local>
2020-07-24 12:24:33 -0700

    none

---
+++ I AM POOR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM POOR
+//
+//  Created by huaminni on 7/24/20.
+//  Copyright © 2020 Huamin Ni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
